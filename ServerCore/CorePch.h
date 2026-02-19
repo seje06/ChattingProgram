cff6932242b275dad506b8d20b538bc3cefa04f3
김세진 <124812852+seje06@users.noreply.github.com>
2026-02-20 00:06:02 +0900

    초기 솔루션 설정

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
