9656ee8cc6edbf620d992a377fa7e4a5e14e61a8
leechunghyun7 <as05100510@gmail.com>
2026-02-23 15:20:49 +0900

    lock codding completion

--- ServerCore/Lock.cpp
+++ ServerCore/Lock.cpp
@@ -5,70 +5,120 @@
 
 void Lock::WriteLock()
 {
-	if (_ownerThreadId.load() == LThreadId)
+	if (_ownerThreadId.load() == LThreadId)  // 같은 스레드에서 write 했을때
 	{
-		_lockState.store(WRITE_STATE);
-		_writeLockCount.fetch_add(1);
+#ifdef _DEBUG
+		ValidateThreadId();
+		if (_readDepthArray[LThreadId] !=0 ) CRASH("READ -> WRITE");
+#endif
+		_writeRecursion.fetch_add(1);
 		return;
 	}
 
+	_writerPending.fetch_add(1);
+
+	int spin = 0;
 	while (true)
 	{
-		ELockState expected = NOTTING_STATE;
+		int expected = NOTTING_STATE;
 		if (_lockState.compare_exchange_strong(expected, WRITE_STATE))
 		{
 			_ownerThreadId.store(LThreadId);
-			_writeLockCount.fetch_add(1);
+			_writeRecursion.store(1);
+			_writerPending.fetch_sub(1);
 			return;
+		}
+		else
+		{
+			if (++spin < 100) _mm_pause(); // 100번까진 저전력으로 돌림
+			else this_thread::yield(); // 이후 쓰레드가 없을때까지 대기상태
 		}
 	}
 }
 
 void Lock::WriteUnLock()
 {
+	if (_ownerThreadId.load() != LThreadId) // 잘못된 write unlock일 경우
+	{
+		CRASH("invalid unlock");
+		return;
+	}
 
+	uint16_t prev = _writeRecursion.fetch_sub(1);
+	if (prev == 0) // 재귀 unlock 언더플로우일때
+	{
+		CRASH("write recursion underflow");
+	}
+	else if (prev == 1)
+	{
+		_ownerThreadId.store(-1);
+		_lockState.exchange(NOTTING_STATE);
+	}
 }
 
 void Lock::ReadLock()
 {
+	// write 에서 같은 스레드가 read로 들어온 경우 통과
 	if (_ownerThreadId.load() == LThreadId)
 	{
-		if (_threadFirstRead[LThreadId] == false)
-		{
-			_threadFirstRead[LThreadId] = true;
-			_readThreadCount.fetch_add(1);
-		}
+#ifdef _DEBUG
+		ValidateThreadId();
+		_readDepthArray[LThreadId].fetch_add(1);
+#endif
 		return;
 	}
 
-	atomic<bool>* canLock = reinterpret_cast<atomic<bool>*>(&_lockState); // WRITE는 0, READ와 NOTTING은 1, 2 기때문에 true면 read하기위해 형변환을 해줌
+	int spin = 0;
 	while (true)
 	{
-		bool expected = true;
-		int expectedId = _ownerThreadId;
-		if (canLock->compare_exchange_strong(expected, true)) // WRITE가 아니면 상태에 READ를 넣어주고 락을 하지않고 진행
+		if (_writerPending.load() > 0)// write를 우선으로 하기위해 write요청이 오면 read는 대기
 		{
-			if (_threadFirstRead[LThreadId] == false)
-			{
-				_threadFirstRead[LThreadId] = true;
-				_readThreadCount.fetch_add(1);
-			}
+			if (++spin < 100) _mm_pause(); // 100번까진 저전력으로 돌림
+			else this_thread::yield(); // 이후 쓰레드가 없을때까지 대기상태
 
-			
-			if (_readThreadCount.load() == 1)
-			{
-				_ownerThreadId.compare_exchange_strong(expectedId, LThreadId);
-			}
-			else
-			{
-				_ownerThreadId.exchange(-2);
-			}
+			continue;
+		}
 
+		int expected = _lockState.load();
+		if (expected == WRITE_STATE) // write중이면 대기
+		{
+			if (++spin < 100) _mm_pause(); // 100번까진 저전력으로 돌림
+			else this_thread::yield(); // 이후 쓰레드가 없을때까지 대기상태
+
+			continue;
+		}
+
+		if (_lockState.compare_exchange_strong(expected, expected + 1))
+		{
+#ifdef _DEBUG
+			ValidateThreadId();
+			_readDepthArray[LThreadId].fetch_add(1);
+#endif
 			return;
+		}
+		else
+		{
+			if (++spin < 500) _mm_pause(); // cas를 시도했다는건 write가 아닐 확률이 더높다 생각하기에 500번까지 늘림
+			else this_thread::yield(); // 이후 쓰레드가 없을때까지 대기상태
+
+			continue;
 		}
 	}
 }
 
 void Lock::ReadUnLock()
 {
+#ifdef _DEBUG
+	ValidateThreadId();
+	auto prev = _readDepthArray[LThreadId].fetch_sub(1);
+	if (prev == 0) CRASH("read depth underflow"); //재귀 unlock 언더플로우일때
+#endif
+	// 같은 스레드의 write에서 들어온 read가 아니면 카운트 감소
+	if (_ownerThreadId.load() != LThreadId)
+	{
+		int prev = _lockState.fetch_sub(1);
+		if (prev <= 0) CRASH("lockState underflow");
+
+		return;
+	}
 }
--- ServerCore/Lock.h
+++ ServerCore/Lock.h
@@ -4,11 +4,11 @@
 
 class Lock
 {
-	enum ELockState : uint8_t
+	enum ELockState : int8_t
 	{
-		WRITE_STATE,
-		READ_STATE,
+		WRITE_STATE = -1,
 		NOTTING_STATE,
+		// 0 > 은 read state
 	};
 
 public:
@@ -18,15 +18,18 @@ public:
 	void ReadUnLock();
 
 private:
-
-
-private:
-	atomic<bool> _canWriteLock = true;
-	atomic<ELockState> _lockState = NOTTING_STATE;
-	atomic<uint16_t> _readThreadCount = 0;
-	atomic<uint16_t> _writeLockCount = 0;
+	atomic<uint16_t> _writerPending = 0;
+	atomic<uint16_t> _writeRecursion = 0;
+	atomic<int> _lockState = NOTTING_STATE;
 	atomic<int> _ownerThreadId = -1;
+	
+#ifdef _DEBUG
+	atomic<uint16_t> _readDepthArray[THREAD_COUNT] = { 0 };
 
-	bool _threadFirstRead[THREAD_COUNT] = {false};
+	void ValidateThreadId() // threadID가 최대치를 넘진않았는지 검증
+	{
+		if (LThreadId < 0 || LThreadId >= THREAD_COUNT) CRASH("Invalid LThreadId");
+	}
+#endif
 };
 
